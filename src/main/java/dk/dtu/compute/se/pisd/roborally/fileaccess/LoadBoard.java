849d704f5b0bc36c7bb9d454e8222f539e0ea98d
saeh00 <saufi2810@gmail.com>
2021-06-11 20:38:36 +0200

    evryting lock off mangler stadig at gemme slejdede kort

--- src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/LoadBoard.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/LoadBoard.java
@@ -46,12 +46,12 @@ public class LoadBoard {
     private static final String JSON_EXT = "json";
 
     public static Board loadBoard(String boardname) {
-        if (boardname == null) {
+        if (boardname == "defaultboard") {
             boardname = DEFAULTBOARD;
-        } else if (boardname == DEFAULTBOARD){
-            boardname = DEFAULTBOARD;
-        } else {
+        } else if (boardname == "board2"){
             boardname = BOARD2;
+        } else {
+            System.out.println("Print noget for hyggens skyld");
         }
 
         ClassLoader classLoader = LoadBoard.class.getClassLoader();
